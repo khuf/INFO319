4ace50fef862c3c0533b97e4281d0cd175f6b061
andal-x <56119942+andal-x@users.noreply.github.com>
2019-11-15 12:16:19 +0100

    Update SentimentPage.js
    Table - må fikse smileys

--- webapp/src/components/pages/sentiment/SentimentPage.js
+++ webapp/src/components/pages/sentiment/SentimentPage.js
@@ -5,6 +5,14 @@ import Box from "@material-ui/core/Box";
 import openSocket from "socket.io-client";
 import Moment from "react-moment";
 import { HashLoader } from "react-spinners";
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import SentimentSatisfiedAltIcon from '@material-ui/icons/SentimentSatisfiedAlt';
+import SentimentVeryDissatisfiedIcon from '@material-ui/icons/SentimentVeryDissatisfied';
 
 import { Grid, InputLabel, Select, MenuItem } from "@material-ui/core/";
 import { withStyles } from "@material-ui/core/styles";
@@ -235,12 +243,57 @@ class SentimentPage extends Component {
     const { classes, theme } = this.props;
     const { window, loader, threshold, messages } = this.state.topic.sentiments;
 
+    const useStyles = makeStyles({
+      root: {
+        width: '100%',
+        overflowX: 'auto',
+      },
+      table: {
+        minWidth: 650,
+      },
+    });
+    
+    function createData(name, sentiment, cf, id) {
+      return { name, sentiment, cf, id };
+    }
+    
+    const rows = [
+      createData('Gobierno Bolivariano activa a más de 7.750 funcionarios ante tormenta', ":)", 4.0, "twitter.com"),
+      createData('Gobierno Bolivariano activa a más', "SentimentSatisfiedAltIcon", 10.0, "twitter.com"),
+      createData('De 7.750 funcionarios ante tormenta', "SentimentVeryDissatisfiedIcon", 1.0, "twitter.com"),
+      createData('ante tormenta', ":(", 14, "twitter.com"),
+    ];
+
     return (
       <NavigationDrawer>
         <Grid container spacing={3}>
           <Grid container item sm={12} xs={12}>
             <Box width="100%">
               <Paper className={classes.paper}></Paper>
+              <Paper className={classes.root}>
+      <Table className={classes.table} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Sentence</TableCell>
+            <TableCell align="right">Sentiment</TableCell>
+            <TableCell align="right">Confidence level</TableCell>
+            <TableCell align="right">Twitter ID</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map(row => (
+            <TableRow key={row.name}>
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.sentiment}</TableCell>
+              <TableCell align="right">{row.cf}</TableCell>
+              <TableCell align="right">{row.id}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </Paper>
             </Box>
           </Grid>
         </Grid>
